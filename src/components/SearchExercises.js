e88c0018f11585a31aed054eeae92cbf29ad2f41
Prajwal Pandey <1805610072@bbdniit.ac.in>
2022-07-07 03:01:53 +0530

    build search Text Feild

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import React from "react";
+import { useEffect, useState } from "react";
+import { Box, Stack, Button, TextField, Typography } from "@mui/material";
+import { width } from "@mui/system";
 const SearchExercises = () => {
-  return (
-    <div>SearchExercises</div>
+  return(
+  <Stack
+    alignItems="center"
+    mt="37px"
+    justifyContent="center"
+    p="20px"
+  >
+    <Typography
+      fontWeight={700} sx={{fontSize: {lg: "44px", xs: "30px"}}}
+      mb="50px" textAlign="center"
+    >
+      Search your burnouts exercises  <br/> you should do.
+    </Typography>
+    <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input:{
+              fontWeight:'700',
+              border:'none',
+              borderRadius:'4px'
+            },
+            width: {lg: "1170px", xs: "350px"},
+            backgroundColor:'#fff',
+            borderRadius:'40px'
+          }}
+           height="76px"
+           value=""
+           onChange={(e) => {}}
+           placeholder="Search your exercises"
+           type="text"
+        />
+        <Button className="search-btn">Search</Button>
+    </Box>
+  </Stack>
   )
-}
+};
 
-export default SearchExercises
+export default SearchExercises;
