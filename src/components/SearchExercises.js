b224de4da166590529730e734225412a4d32f1a1
Prajwal Pandey <1805610072@bbdniit.ac.in>
2022-07-07 20:43:17 +0530

    build search bar

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -2,42 +2,86 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { Box, Stack, Button, TextField, Typography } from "@mui/material";
 import { width } from "@mui/system";
+import { exerciseOptions, fetchData } from "../utlis/fetchData";
+import HorizontalScrollbar from "./HorizontalScrollbar";
+
 const SearchExercises = () => {
-  return(
-  <Stack
-    alignItems="center"
-    mt="37px"
-    justifyContent="center"
-    p="20px"
-  >
-    <Typography
-      fontWeight={700} sx={{fontSize: {lg: "44px", xs: "30px"}}}
-      mb="50px" textAlign="center"
-    >
-      Search your burnouts exercises  <br/> you should do.
-    </Typography>
-    <Box position="relative" mb="72px">
+  const [search, setSearch] = useState("");
+  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+      const fetchExercisesData = async () => {
+        const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+        setBodyParts(['all', ...bodyPartsData]);
+      }
+
+      fetchExercisesData();
+  },[])
+  const handleSearch = async() => {
+    if(search){
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      const SearchedExercises = exercisesData.filter(exercise => exercise.name.toLowerCase().includes(search)
+      ||  exercise.target.toLowerCase().includes(search)
+      || exercise.equipment.toLowerCase().includes(search)
+      ||  exercise.bodyParts.toLowerCase().includes(search)
+      );
+
+      setSearch("");
+      setExercises(SearchedExercises);
+    }
+  }
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Search your burnouts exercises <br /> you should do.
+      </Typography>
+      <Box position="relative" mb="72px">
         <TextField
           sx={{
-            input:{
-              fontWeight:'700',
-              border:'none',
-              borderRadius:'4px'
+            input: {
+              fontWeight: "700",
+              border: "none",
+              borderRadius: "4px",
             },
-            width: {lg: "1170px", xs: "350px"},
-            backgroundColor:'#fff',
-            borderRadius:'40px'
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
           }}
-           height="76px"
-           value=""
-           onChange={(e) => {}}
-           placeholder="Search your exercises"
-           type="text"
+          height="76px"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())}
+          placeholder="Search your exercises"
+          type="text"
         />
-        <Button className="search-btn">Search</Button>
-    </Box>
-  </Stack>
-  )
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+      <Box sx = {{position:'relative', width: '100%', p:'20px'}}>
+          <HorizontalScrollbar data={bodyParts}/>
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
